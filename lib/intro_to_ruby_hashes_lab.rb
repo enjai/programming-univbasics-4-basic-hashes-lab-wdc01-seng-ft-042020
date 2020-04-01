a8a28641961bfa021afba386fab26bed0f1e57cb
Injae Lee <ilee.injae@gmail.com>
2020-04-01 03:42:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,16 @@ def new_hash
 end
 
 def my_hash
-  Hash.new{:name}
+  Hash.new[:name] = "Injae"
 end
 
 def pioneer
+  Hash.new[:name] = "Grace Hopper"
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+ Hash.new[:id] = number 
   # return a hash with a key :id assigned to the provided number
 end
