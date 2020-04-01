e493d3aad443b0b0f107a4d7e832ce5cc5638512
Injae Lee <ilee.injae@gmail.com>
2020-04-01 03:46:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def my_hash
 end
 
 def pioneer
-  Hash.new[:name] => "Grace Hopper"
+  pioneer = {:name => "Grace Hopper"}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
